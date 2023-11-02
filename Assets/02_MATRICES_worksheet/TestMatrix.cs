3c09b0d19583fb22e859a8a09d24fc2d6b39386b
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-11-02 14:35:12 +0800

    Finish Q2

--- Assets/02_MATRICES_worksheet/TestMatrix.cs
+++ Assets/02_MATRICES_worksheet/TestMatrix.cs
@@ -9,5 +9,24 @@ public class TestMatrix : MonoBehaviour
     {
         mat.SetIdentity();
         mat.Print();
+        
+        Question2();
+    }
+
+    void Question2()
+    {
+        var matA = new HMatrix2D(
+            1f, 2f, 1f,
+            0f, 1f, 0f,
+            2f, 3f, 4f
+        );
+        var matB = new HMatrix2D(
+            2f, 5f, 1f,
+            6f, 7f, 1f,
+            1f, 8f, 1f
+        );
+
+        (matA * matB).Print();
+        
     }
 }
