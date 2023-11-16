e07d1a822755150b0cd1b2f7427529137a9c5f26
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-11-16 14:39:19 +0800

    First part done

--- Assets/03_KINEMATICS_Worksheet/Part 1/Force/FirstLaw.cs
+++ Assets/03_KINEMATICS_Worksheet/Part 1/Force/FirstLaw.cs
@@ -1,21 +1,23 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class FirstLaw : MonoBehaviour
-// {
-//     public Vector3 force;
-//     Rigidbody rb;
+public class FirstLaw : MonoBehaviour
+{
+    public Vector3 force;
+    Rigidbody rb;
 
-//     void Start()
-//     {
-//         rb = // your code here;
-//         rb.// your code here;
-//     }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+        rb.AddForce(Vector3.up * 100f);
+        rb.AddForce(Vector3.right * 300f);
+    }
 
-//     void FixedUpdate()
-//     {
-//         Debug.Log(transform.position);
-//     }
-// }
+    void FixedUpdate()
+    {
+        Debug.Log(transform.position);
+    }
+}
 
