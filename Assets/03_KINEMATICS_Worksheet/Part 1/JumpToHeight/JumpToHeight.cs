b5e89e03c170e1ceab7460e19718ed52ffd5333b
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-11-16 15:04:56 +0800

    jump

--- Assets/03_KINEMATICS_Worksheet/Part 1/JumpToHeight/JumpToHeight.cs
+++ Assets/03_KINEMATICS_Worksheet/Part 1/JumpToHeight/JumpToHeight.cs
@@ -1,37 +1,39 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class JumpToHeight : MonoBehaviour
-// {
-//     public float Height = 1f;
-//     Rigidbody rb;
+public class JumpToHeight : MonoBehaviour
+{
+    public float Height = 1f;
+    Rigidbody rb;
 
-//     private void Start()
-//     {
-//         rb = /*your code here*/;
-//     }
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-//     void Jump()
-//     {
-//         // v*v = u*u + 2as
-//         // u*u = v*v - 2as
-//         // u = sqrt(v*v - 2as)
-//         // v = 0, u = ?, a = Physics.gravity, s = Height
+    void Jump()
+    {
+        // v*v = u*u + 2as
+        // u*u = v*v - 2as
+        // u = sqrt(v*v - 2as)
+        // v = 0, u = ?, a = Physics.gravity, s = Height
 
-//         float u = Mathf.Sqrt(/*your code here*/);
-//         rb.velocity = new Vector3(/*your code here*/);
+        float u = Mathf.Sqrt(
+            rb.velocity.y*rb.velocity.y + 2 * 10 * Height
+            );
+        rb.velocity = new Vector3(rb.velocity.x,u);
 
-//         //float jumpForce = Mathf.Sqrt(-2 * Physics2D.gravity.y * Height);
-//         //rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-//     }
+        // float jumpForce = Mathf.Sqrt(-2 * Physics2D.gravity.y * Height);
+        // rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+    }
 
-//     private void Update()
-//     {
-//         if(Input.GetKeyDown(KeyCode.Space))
-//         {
-//             /*your code here*/();
-//         }
-//     }
-// }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+}
 
