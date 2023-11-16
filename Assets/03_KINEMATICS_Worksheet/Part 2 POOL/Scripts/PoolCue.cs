d4d1530e726af4b7e1b9eb94e8c70f0a2927a241
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-11-16 15:52:21 +0800

    did stuff

--- Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/PoolCue.cs
+++ Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/PoolCue.cs
@@ -1,58 +1,58 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-//
-// public class PoolCue : MonoBehaviour
-// {
-// 	public LineFactory lineFactory;
-// 	public GameObject ballObject;
-//
-// 	private Line drawnLine;
-// 	private Ball2D ball;
-//
-// 	private void Start()
-// 	{
-// 		ball = ballObject.GetComponent<Ball2D>();
-// 	}
-//
-// 	void Update()
-// 	{
-// 		if (Input.GetMouseButtonDown(0))
-// 		{
-// 			var startLinePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Start line drawing
-// 			if (ball != null && ball.IsCollidingWith(/*your code here*/))
-// 			{
-// 				drawnLine = /*your code here*/;
-// 				drawnLine.EnableDrawing(true);
-// 			}
-// 		}
-// 		else if (Input.GetMouseButtonUp(0) && drawnLine != null)
-// 		{
-// 			drawnLine.EnableDrawing(false);
-//
-// 			//update the velocity of the white ball.
-// 			HVector2D v = new HVector2D(/*your code here*/);
-// 			ball./*your code here*/ = v;
-//
-// 			drawnLine = null; // End line drawing            
-// 		}
-//
-// 		if (drawnLine != null)
-// 		{
-// 			drawnLine.end = /*your code here*/; // Update line end
-// 		}
-// 	}
-//
-// 	/// <summary>
-// 	/// Get a list of active lines and deactivates them.
-// 	/// </summary>
-// 	public void Clear()
-// 	{
-// 		var activeLines = lineFactory.GetActive();
-//
-// 		foreach (var line in activeLines)
-// 		{
-// 			line.gameObject.SetActive(false);
-// 		}
-// 	}
-// }
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolCue : MonoBehaviour
+{
+	public LineFactory lineFactory;
+	public GameObject ballObject;
+
+	private Line drawnLine;
+	private Ball2D ball;
+
+	private void Start()
+	{
+		ball = ballObject.GetComponent<Ball2D>();
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			var startLinePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Start line drawing
+			if (ball != null && ball.IsCollidingWith(startLinePos.x,startLinePos.y))
+			{
+				drawnLine = lineFactory.GetLine(ball.Position.ToUnityVector2(),startLinePos,10,Color.gray);
+				drawnLine.EnableDrawing(true);
+			}
+		}
+		// else if (Input.GetMouseButtonUp(0) && drawnLine != null)
+		// {
+		// 	drawnLine.EnableDrawing(false);
+		//
+		// 	//update the velocity of the white ball.
+		// 	HVector2D v = new HVector2D(/*your code here*/);
+		// 	ball./*your code here*/ = v;
+		//
+		// 	drawnLine = null; // End line drawing            
+		// }
+
+		// if (drawnLine != null)
+		// {
+		// 	drawnLine.end = /*your code here*/; // Update line end
+		// }
+	}
+
+	/// <summary>
+	/// Get a list of active lines and deactivates them.
+	/// </summary>
+	public void Clear()
+	{
+		var activeLines = lineFactory.GetActive();
+
+		foreach (var line in activeLines)
+		{
+			line.gameObject.SetActive(false);
+		}
+	}
+}
--- Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
+++ Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
@@ -1,12 +1,12 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-//
-// public class Util
-// {
-//     public static float FindDistance(HVector2D p1, HVector2D p2)
-//     {
-//         return /*your code here*/;
-//     }
-// }
-//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util
+{
+    public static float FindDistance(HVector2D p1, HVector2D p2)
+    {
+        return ( p1-p2).Magnitude();
+    }
+}
+
