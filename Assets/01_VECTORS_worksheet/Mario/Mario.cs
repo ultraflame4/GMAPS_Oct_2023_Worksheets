24ab7023b9dd47c18234261d082bdb3ae5b00b51
ultr42 <ultr42@skiff.com>
2023-12-06 22:17:02 +0800

    Added comments

--- Assets/01_VECTORS_worksheet/Mario/Mario.cs
+++ Assets/01_VECTORS_worksheet/Mario/Mario.cs
@@ -16,16 +16,24 @@ public class Mario : MonoBehaviour
 
     void FixedUpdate()
     {
+        // Direction of gravity which will be vector from player to the planet
         gravityDir = (planet.position - transform.position);
+        // Normalised direction of gravity
         gravityNorm = gravityDir.normalized;
         
+        // Perpendicular direction to gravity will be the direction of movement
         moveDir = new Vector3(-gravityNorm.y, gravityNorm.x, 0).normalized;
+        // Move in the direction of movement with the force and if the player is pressing the up/down w/s keys
         rb.AddForce(moveDir * force * Input.GetAxisRaw("Vertical"));
+        // Apply gravity to attract player towards the planet
         rb.AddForce(gravityDir * gravityStrength);
 
+        // Get angle of gravity relative to the y axis. Using vector3.down because it was rotating the other way
         float angle = Vector3.SignedAngle(Vector3.down, gravityNorm, Vector3.forward);
+        // Rotate the player so they stay upright
         rb.MoveRotation(Quaternion.Euler(0,0,angle));
         
+        // Draw vectors
         DebugExtension.DebugArrow(transform.position, gravityDir, Color.red);
         DebugExtension.DebugArrow(transform.position, moveDir, Color.blue);
         
